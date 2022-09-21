3430574f8037e2834581821929c506c1d80bca5a
Manupatino16 <manuelap1604@gmail.com>
2022-09-21 18:36:25 -0500

    bakery shop

---
+++ src/controllers/aprendiendoarraymetodos.js
@@ -0,0 +1,145 @@
+let productos=[
+
+    {
+        nombre:"FINANCIER",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto4.webp?alt=media&token=d3a1ef6f-b4f5-4322-8901-5599e03b1d15","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto4.4.webp?alt=media&token=1023eae6-592c-40aa-914b-3517839fb166"],
+        precio: 8900,
+        popularidad:4,
+        descripcion:"Se trata de un suave y jugoso bizcocho a base de almendra llamado financier (literalmente «financiero»). Es un postre muy versátil, ya que se pueden añadir infinidad de sabores a la receta base." 
+    },
+    {
+        nombre:"CROISSANT DE JAMÓN & QUESO",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto2.webp?alt=media&token=ed4f8b95-d347-4bab-87aa-1a5f225e99e6","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto2.2.webp?alt=media&token=bb6f3794-9937-4eff-8080-5574541fb8ec"],
+        precio:18900,
+        popularidad:5,
+        descripcion:"Un croissant elaborado con crujiente masa danesa, relleno de jamón y queso emmental y cubierto de crujiente queso emmental rallado. Un apetitoso y delicioso croissant que viene en cajas con 4 bolsas de 12 unidades cada una, ideal para hornear bandejas enteras, así como para ahorrar espacio en el congelador y conservando el producto en óptimas condiciones." 
+    },
+    {
+        nombre:"GALLETAS",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto3.webp?alt=media&token=e7def716-e451-49e3-bef3-750e2414bf3c","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto3.3.webp?alt=media&token=346215d7-e7cf-496b-a64a-0789a64858c7"],
+        precio:19400,
+        popularidad:4,
+        descripcion:"Nuestras famosas chocochips pero mucho más grandes, más amelcochadas y con más chocolate de lo normal. Caja pequeña con seis galletas variadas de dos sabores. Es el detalle perfecto para sorprender a alguien especial."
+    },
+    {
+        nombre:"PAN ROLL 5 GRANOS",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto5.5.webp?alt=media&token=1595dded-03a4-4897-bd81-e46f3ab5998f","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto5.webp?alt=media&token=dd19e861-fee7-4394-a12b-85559edcfc8d"],
+        precio:16000,
+        popularidad:5,
+        descripcion:"Toda la fibra de los cereales concentrados en un solo producto. Pan integral elaborado con cuchuco de trigo, linaza, ajonjolí, salvado y cubierto con hojuelas de avena." 
+    },
+    {
+        nombre:"TARTALETA DE ALMENDRA Y AGRAZ",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto6.webp?alt=media&token=018f09e8-737b-4fff-b562-1d5cef827b05","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto6.6.webp?alt=media&token=e7f6c179-ec41-4820-ab91-cd72354dc93b"],
+        precio:53400,
+        popularidad:4,
+        descripcion:"No hay nada como una torta riquísima para acompañar un té o un café. Ahora que está cambiando el clima ya empezamos a tomar cosas calientes y esta torta de almendras es el acompañamiento perfecto. ¡Tiene mucho sabor a almendras" 
+    },
+    {
+        nombre:"PAN AU CHOCOLAT",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto7.webp?alt=media&token=405323a4-5a07-45f3-b05c-7b8449481b7d","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto7.7.webp?alt=media&token=3bc6b2a3-114c-4151-8339-9c50bcaa8a22"],
+        precio:17800,
+        popularidad:4,
+        descripcion:"Si te gustan los croissants y el chocolate, te presento a tu nueva obsesión: el pain au chocolat. Este pancito se hace con la misma masa del croissant y se rellena con chocolate para darle un toque delicioso. Una vez que lo pruebes, lo vas a querer hacer todos los fines de semana." 
+    },
+    {
+        nombre:"PAN ROLL CHALLAH",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto8.8.webp?alt=media&token=387e2776-42c2-4e15-951b-05a074547613","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto8.webp?alt=media&token=c52be226-b510-4baf-8930-68a91dafd4ad"],
+        precio:10200,
+        popularidad:5,
+        descripcion:"El pan jalá se puede elaborar en toda una variedad de formas y tamaños (en espiral, en forma de llave, de pájaro, de libro e incluso de flor), cada uno con su propio significado. Los trenzados pueden constar de tres, cuatro o seis cabos entre los más habituales, y simbolizan el amor, pues su diseño recuerda a unos brazos entrelazados. Cuando se trenza en tres, simboliza la verdad, la paz y el amor." 
+    },
+    {
+        nombre:"TARTALETA DE FRESA, ALMENDRA Y CREMA PASTELERA",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto9.webp?alt=media&token=f64de448-fa64-4431-91dc-0083a4329576","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto9.9.webp?alt=media&token=4c070904-9d68-424b-bd21-5976dbc41846"],
+        precio:53400,
+        popularidad:4,
+        descripcion:"Con la llegada de la primavera, los escaparates de las pastelerías francesas se llenan de esta tarta de fresas y en casi cualquier brasería, puedes encontrarla como el postre especial del día. Se trata, sin duda,  del postre por excelencia de esta estación." 
+    },
+    {
+        nombre:"PAN INTEGRAL DE NUECES Y UVAS",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto10.webp?alt=media&token=00edc9dd-9306-4505-80af-2c7085244d12","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto10.10.webp?alt=media&token=c6323a02-08b2-4159-bb56-f088cb9af2fc"],
+        precio:19800,
+        popularidad:3,
+        descripcion:"La combinación de harina integral con nueces es ideal para obtener un pan más equilibrado desde el punto de vista nutricional y a la vez es absolutamente delicioso, con una sabor a nueces muy interesante y con una miga bien alveolada. Este pan es ideal para consumir como tostadas en el desayuno, merienda, o cómo acompañamiento en las comidas" 
+    },
+    {
+        nombre:"CROISSANT DE ALMENDRA",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto11.webp?alt=media&token=9aa96db4-f312-43bc-86ba-99c0e89991a7","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto11.11.webp?alt=media&token=3ed776ce-cbe6-4cfb-ab30-dc74fbd4a8c8"],
+        precio:18900,
+        popularidad:4,
+        descripcion:"Crocantes por fuera, muy laminados y con relleno de almendras, te presento a tus nuevos croissants favoritos: los croissants de almendras. Si nunca los has comido, de todas maneras los vas a amar con todo tu corazón." 
+    },
+    {
+        nombre:"TORTA DE ZANAHORIA",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto1.1.jpg?alt=media&token=e0cb936b-7d3c-46d8-bdc6-9c9603264ad4","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto1.webp?alt=media&token=e4533017-fe82-4c16-994c-015c77293b99"],
+        precio:55100,
+        popularidad:3,
+        descripcion:"Si deseas endulzar tu paladar de una manera saludable, prepara esta exquisita torta de zanahoria que te dejará con las ganas de repetir."
+    },
+    {
+        nombre:"TORTA DE CHOCOLATE",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto12.webp?alt=media&token=8e423092-2dc2-485d-9e0b-6481f34d2977","https://firebasestorage.googleapis.com/v0/b/tiendampb.appspot.com/o/producto12.12.webp?alt=media&token=499b633a-9f6b-4235-81c2-78a369cbdad6"],
+        precio:90400,
+        popularidad:5,
+        descripcion:"Torta de chocolate con relleno y cubierta de ganache (crema de leche, cobertura semiamarga)" 
+    }
+
+]
+
+let fila=document.getElementById("fila")
+
+//RECORRIENDO EL ARREGLO
+productos.forEach(function(producto){
+    
+    //creando un div desde JS
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    //creando una tarjeta
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","shadow","text-center","h-100","p-5","bg-secondary")
+
+    //creando una IMAGEN
+    let imagen=document.createElement("img")
+    imagen.classList.add("img-fluid","w-100")
+    imagen.src=producto.fotos[0]
+
+    //DETECTANDO EVENTO DE MOUSE
+    imagen.addEventListener("mouseover",function(){
+        imagen.src=producto.fotos[1]
+    })
+    imagen.addEventListener("mouseleave",function(){
+        imagen.src=producto.fotos[0]
+    })
+
+    //crear el nombre
+    let nombre=document.createElement("h3")
+    nombre.classList.add("fw-bold")
+    nombre.textContent=producto.nombre
+
+    //crear el precio
+    let precio=document.createElement("h2")
+    precio.textContent="$"+producto.precio
+
+    //creando la descripcion
+    let descripcion=document.createElement("p")
+     descripcion.classList.add("text-danger","d-none")
+     descripcion.textContent=producto.descripcion
+
+    //crear la popularidad
+    let popularidad=document.createElement("h4")
+    popularidad.textContent=producto.popularidad
+    
+
+    //definiendo padres e hijos
+    tarjeta.appendChild(imagen)
+    tarjeta.appendChild(nombre)
+    tarjeta.appendChild(precio)
+    tarjeta.appendChild(descripcion)
+    tarjeta.appendChild(popularidad)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+
+
+})
